0da1056fb33517459bdf5e5e215e63e65fd6b304
233404050209-blip <233404050209@ldxh.edu.vn>
2026-09-08 22:03:08 +0700

    feat: them API counter va API info

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
-import os # them dong nay len dau file, truoc "from flask import Flask"
+import os
+import datetime
+import platform
 from flask import Flask
-import datetime, platform
 app = Flask(__name__)
-visit_count = 0 # Bien dem luu trong RAM cua container
-
-@app.route("/api/info")
-def info():
- ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
- return {
-"sinh_vien": ten_sinh_vien,
-"nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
-}
+# Bien dem luu trong RAM cua container
+visit_count = 0
 @app.route("/api/counter")
 def counter():
- global visit_count
-visit_count += 1 
-return {
-  "so_lan_truy_cap": visit_count,
-   "ghi_chu": "So nay se MAT khi container khoi dong lai!"
- }
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get(
+        "STUDENT_NAME",
+        "Chua dat bien moi truong"
+    )
+    return {
+        "sinh_vien": ten_sinh_vien,
+        "thoi_gian": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "he_dieu_hanh": platform.system(),
+        "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
